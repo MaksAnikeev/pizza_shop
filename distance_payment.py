1b13e6a57b9ead607622e53a75349200bb2ae497
MaksAnikeev <anikeev.maks@rambler.ru>
2023-04-30 10:11:39 +0300

    Divided function to two files

---
+++ distance_payment.py
@@ -0,0 +1,166 @@
+import environs
+import requests
+import time
+from geopy import distance
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, LabeledPrice
+from telegram.ext import (Filters, MessageHandler, Updater, PreCheckoutQueryHandler)
+from moltin import get_entrie, get_token
+from pprint import pprint
+
+
+def fetch_coordinates(api_yandex_key, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": api_yandex_key,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
+
+
+def get_user_dictance(user):
+    return user['distance']
+
+
+def get_min_distance(client_coordinates, pizzerias_params):
+    distance_to_client = []
+    for pizzeria in pizzerias_params:
+        pizzeria_longitude = pizzeria['pizzeria_longitude']
+        pizzeria_latitude = pizzeria['pizzeria_latitude']
+        pizzeria_address = (pizzeria_longitude, pizzeria_latitude)
+        client_distance = round(distance.distance(client_coordinates, pizzeria_address).km, 2)
+        client_params = {'pizzeria_address': pizzeria['pizzeria_address'],
+                         'distance': client_distance,
+                         'pizzeria_id': pizzeria['id']}
+        distance_to_client.append(client_params)
+    distance_params = min(distance_to_client, key=get_user_dictance)
+    pizzeria_full_address = distance_params['pizzeria_address']
+    distance_to_client = distance_params['distance']
+    pizzeria_id = distance_params['pizzeria_id']
+    return pizzeria_full_address, pizzeria_id, distance_to_client
+
+
+def send_alarm_clock_message(context):
+    job = context.job
+    alarm_clock_message = 'Приятного аппетита! Надеемся что пицца к вам пришла вовремя ' \
+                          'и вы уже наслаждаетесь ее вкусом! \n\n' \
+                          'Если это вдруг не так, то свяжитесь с нами по этому телефону +7 978 656 44 55' \
+                          'и мы вам вернем деньги.'
+    keyboard = [[InlineKeyboardButton("Назад к корзине",
+                                      callback_data='back_to_cart')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(
+        chat_id=job.context,
+        text=alarm_clock_message,
+        reply_markup=reply_markup)
+    return 'CART'
+
+
+def one_hour_timer(update, context):
+    time.sleep(4)
+    due = 60
+    keyboard = [[InlineKeyboardButton("Назад к корзине",
+                                      callback_data='back_to_cart')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.delete_message(
+        chat_id=update.effective_chat.id,
+        message_id=context.user_data['message_id']
+    )
+
+    id = context.user_data['pizzeria_id']
+    slug = 'pizzeria'
+
+    env = environs.Env()
+    env.read_env()
+    client_id = env.str("CLIENT_ID")
+    client_secret = env.str("CLIENT_SECRET")
+    access_token, token_expires = get_token(client_id, client_secret)
+
+    courier_tg_id = get_entrie(access_token, slug, id)['data']['telegram_id']
+    client_address = context.user_data['client_address']
+    context.bot.send_message(
+        chat_id=courier_tg_id,
+        text=f'Это видит только курьер \n'
+             f'Адрес клиента: {client_address}',
+    )
+    context.bot.send_location(
+        chat_id=courier_tg_id,
+        latitude=context.user_data['client_longitude'],
+        longitude=context.user_data['client_longitude'])
+
+    timer_message = context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Оплата успешно произведена \n '
+             'Мы заботимся о своих клиентах чтобы они получали свежеприготовленную пиццу.\n'
+             'Если через 1 час вам не привезут пиццу, то мы вернем вам деньги',
+        reply_markup=reply_markup)
+    context.user_data['timer_message_id'] = timer_message.message_id
+    context.job_queue.run_once(send_alarm_clock_message, due, context=update.effective_chat.id)
+    return 'CART'
+
+
+def send_payment(update, context):
+    chat_id = update.effective_chat.id
+    title = "Оплата заказа в пицца-Макс"
+    description = f"Стоимость заказа {context.user_data['cart_sum_num']}руб \n" \
+                  f"Стоимость доставки - {context.user_data['delivery_tax']}руб"
+    payload = "Custom-Payload"
+    currency = "RUB"
+    price = int(context.user_data['cart_sum_num']) + int(context.user_data['delivery_tax'])
+    prices = [LabeledPrice("Test", price * 10)]
+
+    env = environs.Env()
+    env.read_env()
+    provider_token = env.str("PAYMENT_UKASSA_TOKEN")
+
+    context.bot.send_invoice(
+        chat_id, title, description, payload, provider_token, currency, prices)
+
+
+def precheckout_callback(update, context):
+    query = update.pre_checkout_query
+    if query.invoice_payload != 'Custom-Payload':
+        query.answer(ok=False, error_message="Something went wrong...")
+    else:
+        query.answer(ok=True)
+
+
+def successful_payment(update, context):
+    if context.user_data['delivery_choice'] == 'delivery':
+        return one_hour_timer(update, context)
+    else:
+        message = 'Оплата успешно произведена, ждем вас за пиццей'
+        keyboard = [[InlineKeyboardButton("Назад к корзине",
+                                          callback_data='back_to_cart')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=message,
+            reply_markup=reply_markup)
+        return 'CART'
+
+
+if __name__ == '__main__':
+    env = environs.Env()
+    env.read_env()
+
+    token = env.str("TG_BOT_TOKEN")
+    provider_token = env.str("PAYMENT_UKASSA_TOKEN")
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+
+    api_yandex_key = env('API_YANDEX_KEY')
+
+    dispatcher.add_handler(PreCheckoutQueryHandler(precheckout_callback))
+    dispatcher.add_handler(MessageHandler(Filters.successful_payment, successful_payment))
+
+    updater.start_polling()
+    updater.idle()
--- pizza_bot.py
+++ pizza_bot.py
@@ -4,11 +4,11 @@ from textwrap import dedent
 import environs
 import redis
 import requests
-import time
-from geopy import distance
+# import time
+# from geopy import distance
 from datetime import datetime
 from more_itertools import chunked
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, LabeledPrice
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram.ext import (CallbackQueryHandler, CommandHandler, Filters,
                           MessageHandler, Updater, PreCheckoutQueryHandler)
 from moltin import (get_token, get_product_params,
@@ -16,6 +16,10 @@ from moltin import (get_token, get_product_params,
                     get_products_names, get_products_params, add_item_to_cart,
                     get_products_from_cart, get_cart_params, delete_item_from_cart,
                     get_entries, fill_fieds, get_entrie)
+from distance_payment import (fetch_coordinates, get_min_distance, send_payment,
+                              precheckout_callback, successful_payment)
+
+
 from pprint import pprint
 
 _database = None
@@ -562,131 +566,131 @@ def get_database_connection():
     return _database
 
 
-def fetch_coordinates(api_yandex_key, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": api_yandex_key,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
-
-def get_user_dictance(user):
-    return user['distance']
-
-
-def get_min_distance(client_coordinates, pizzerias_params):
-    distance_to_client = []
-    for pizzeria in pizzerias_params:
-        pizzeria_longitude = pizzeria['pizzeria_longitude']
-        pizzeria_latitude = pizzeria['pizzeria_latitude']
-        pizzeria_address = (pizzeria_longitude, pizzeria_latitude)
-        client_distance = round(distance.distance(client_coordinates, pizzeria_address).km, 2)
-        client_params = {'pizzeria_address': pizzeria['pizzeria_address'],
-                         'distance': client_distance,
-                         'pizzeria_id': pizzeria['id']}
-        distance_to_client.append(client_params)
-    distance_params = min(distance_to_client, key=get_user_dictance)
-    pizzeria_full_address = distance_params['pizzeria_address']
-    distance_to_client = distance_params['distance']
-    pizzeria_id = distance_params['pizzeria_id']
-    return pizzeria_full_address, pizzeria_id, distance_to_client
-
-
-def send_alarm_clock_message(context):
-    job = context.job
-    alarm_clock_message = 'Приятного аппетита! Надеемся что пицца к вам пришла вовремя ' \
-                          'и вы уже наслаждаетесь ее вкусом! \n\n' \
-                          'Если это вдруг не так, то свяжитесь с нами по этому телефону +7 978 656 44 55' \
-                          'и мы вам вернем деньги.'
-    keyboard = [[InlineKeyboardButton("Назад к корзине",
-                                      callback_data='back_to_cart')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    context.bot.send_message(
-        chat_id=job.context,
-        text=alarm_clock_message,
-        reply_markup=reply_markup)
-    return 'CART'
-
-
-def one_hour_timer(update, context):
-    time.sleep(4)
-    due = 60
-    keyboard = [[InlineKeyboardButton("Назад к корзине",
-                                      callback_data='back_to_cart')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    context.bot.delete_message(
-        chat_id=update.effective_chat.id,
-        message_id=context.user_data['message_id']
-    )
-
-    id = context.user_data['pizzeria_id']
-    slug = 'pizzeria'
-    courier_tg_id = get_entrie(access_token, slug, id)['data']['telegram_id']
-    client_address = context.user_data['client_address']
-    context.bot.send_message(
-        chat_id=courier_tg_id,
-        text=f'Это видит только курьер \n'
-             f'Адрес клиента: {client_address}',
-    )
-    context.bot.send_location(
-        chat_id=courier_tg_id,
-        latitude=context.user_data['client_longitude'],
-        longitude=context.user_data['client_longitude'])
-
-    timer_message = context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text='Оплата успешно произведена \n '
-             'Мы заботимся о своих клиентах чтобы они получали свежеприготовленную пиццу.\n'
-             'Если через 1 час вам не привезут пиццу, то мы вернем вам деньги',
-        reply_markup=reply_markup)
-    context.user_data['timer_message_id'] = timer_message.message_id
-    context.job_queue.run_once(send_alarm_clock_message, due, context=update.effective_chat.id)
-    return 'CART'
-
-
-def send_payment(update, context):
-    chat_id = update.effective_chat.id
-    title = "Оплата заказа в пицца-Макс"
-    description = f"Стоимость заказа {context.user_data['cart_sum_num']}руб \n" \
-                  f"Стоимость доставки - {context.user_data['delivery_tax']}руб"
-    payload = "Custom-Payload"
-    currency = "RUB"
-    price = int(context.user_data['cart_sum_num']) + int(context.user_data['delivery_tax'])
-    prices = [LabeledPrice("Test", price * 10)]
-    context.bot.send_invoice(
-        chat_id, title, description, payload, provider_token, currency, prices)
+# def fetch_coordinates(api_yandex_key, address):
+#     base_url = "https://geocode-maps.yandex.ru/1.x"
+#     response = requests.get(base_url, params={
+#         "geocode": address,
+#         "apikey": api_yandex_key,
+#         "format": "json",
+#     })
+#     response.raise_for_status()
+#     found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+#
+#     if not found_places:
+#         return None
+#
+#     most_relevant = found_places[0]
+#     lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+#     return lon, lat
+#
+# def get_user_dictance(user):
+#     return user['distance']
+#
+#
+# def get_min_distance(client_coordinates, pizzerias_params):
+#     distance_to_client = []
+#     for pizzeria in pizzerias_params:
+#         pizzeria_longitude = pizzeria['pizzeria_longitude']
+#         pizzeria_latitude = pizzeria['pizzeria_latitude']
+#         pizzeria_address = (pizzeria_longitude, pizzeria_latitude)
+#         client_distance = round(distance.distance(client_coordinates, pizzeria_address).km, 2)
+#         client_params = {'pizzeria_address': pizzeria['pizzeria_address'],
+#                          'distance': client_distance,
+#                          'pizzeria_id': pizzeria['id']}
+#         distance_to_client.append(client_params)
+#     distance_params = min(distance_to_client, key=get_user_dictance)
+#     pizzeria_full_address = distance_params['pizzeria_address']
+#     distance_to_client = distance_params['distance']
+#     pizzeria_id = distance_params['pizzeria_id']
+#     return pizzeria_full_address, pizzeria_id, distance_to_client
+#
+#
+# def send_alarm_clock_message(context):
+#     job = context.job
+#     alarm_clock_message = 'Приятного аппетита! Надеемся что пицца к вам пришла вовремя ' \
+#                           'и вы уже наслаждаетесь ее вкусом! \n\n' \
+#                           'Если это вдруг не так, то свяжитесь с нами по этому телефону +7 978 656 44 55' \
+#                           'и мы вам вернем деньги.'
+#     keyboard = [[InlineKeyboardButton("Назад к корзине",
+#                                       callback_data='back_to_cart')]]
+#     reply_markup = InlineKeyboardMarkup(keyboard)
+#     context.bot.send_message(
+#         chat_id=job.context,
+#         text=alarm_clock_message,
+#         reply_markup=reply_markup)
+#     return 'CART'
+#
+#
+# def one_hour_timer(update, context):
+#     time.sleep(4)
+#     due = 60
+#     keyboard = [[InlineKeyboardButton("Назад к корзине",
+#                                       callback_data='back_to_cart')]]
+#     reply_markup = InlineKeyboardMarkup(keyboard)
+#     context.bot.delete_message(
+#         chat_id=update.effective_chat.id,
+#         message_id=context.user_data['message_id']
+#     )
+#
+#     id = context.user_data['pizzeria_id']
+#     slug = 'pizzeria'
+#     courier_tg_id = get_entrie(access_token, slug, id)['data']['telegram_id']
+#     client_address = context.user_data['client_address']
+#     context.bot.send_message(
+#         chat_id=courier_tg_id,
+#         text=f'Это видит только курьер \n'
+#              f'Адрес клиента: {client_address}',
+#     )
+#     context.bot.send_location(
+#         chat_id=courier_tg_id,
+#         latitude=context.user_data['client_longitude'],
+#         longitude=context.user_data['client_longitude'])
+#
+#     timer_message = context.bot.send_message(
+#         chat_id=update.effective_chat.id,
+#         text='Оплата успешно произведена \n '
+#              'Мы заботимся о своих клиентах чтобы они получали свежеприготовленную пиццу.\n'
+#              'Если через 1 час вам не привезут пиццу, то мы вернем вам деньги',
+#         reply_markup=reply_markup)
+#     context.user_data['timer_message_id'] = timer_message.message_id
+#     context.job_queue.run_once(send_alarm_clock_message, due, context=update.effective_chat.id)
+#     return 'CART'
 
 
-def precheckout_callback(update, context):
-    query = update.pre_checkout_query
-    if query.invoice_payload != 'Custom-Payload':
-        query.answer(ok=False, error_message="Something went wrong...")
-    else:
-        query.answer(ok=True)
+# def send_payment(update, context):
+#     chat_id = update.effective_chat.id
+#     title = "Оплата заказа в пицца-Макс"
+#     description = f"Стоимость заказа {context.user_data['cart_sum_num']}руб \n" \
+#                   f"Стоимость доставки - {context.user_data['delivery_tax']}руб"
+#     payload = "Custom-Payload"
+#     currency = "RUB"
+#     price = int(context.user_data['cart_sum_num']) + int(context.user_data['delivery_tax'])
+#     prices = [LabeledPrice("Test", price * 10)]
+#     context.bot.send_invoice(
+#         chat_id, title, description, payload, provider_token, currency, prices)
 
 
-def successful_payment(update, context):
-    if context.user_data['delivery_choice'] == 'delivery':
-        return one_hour_timer(update, context)
-    else:
-        message = 'Оплата успешно произведена, ждем вас за пиццей'
-        keyboard = [[InlineKeyboardButton("Назад к корзине",
-                                          callback_data='back_to_cart')]]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=message,
-            reply_markup=reply_markup)
-        return 'CART'
+# def precheckout_callback(update, context):
+#     query = update.pre_checkout_query
+#     if query.invoice_payload != 'Custom-Payload':
+#         query.answer(ok=False, error_message="Something went wrong...")
+#     else:
+#         query.answer(ok=True)
+
+
+# def successful_payment(update, context):
+#     if context.user_data['delivery_choice'] == 'delivery':
+#         return one_hour_timer(update, context)
+#     else:
+#         message = 'Оплата успешно произведена, ждем вас за пиццей'
+#         keyboard = [[InlineKeyboardButton("Назад к корзине",
+#                                           callback_data='back_to_cart')]]
+#         reply_markup = InlineKeyboardMarkup(keyboard)
+#         context.bot.send_message(
+#             chat_id=update.effective_chat.id,
+#             text=message,
+#             reply_markup=reply_markup)
+#         return 'CART'
 
 # query = update.callback_query
 #     context.bot.edit_message_text(
@@ -710,6 +714,7 @@ if __name__ == '__main__':
     price_list_id = env.str("PRICE_LIST_ID")
     api_yandex_key = env('API_YANDEX_KEY')
     access_token, token_expires = get_token(client_id, client_secret)
+    dispatcher.bot_data['provider_token'] = provider_token
     dispatcher.bot_data['access_token'] = access_token
     dispatcher.bot_data['token_expires'] = token_expires
     dispatcher.bot_data['price_list_id'] = price_list_id
